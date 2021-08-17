1abd7dbdba0c54fd879d984cf2219f155884fcae
Kerem Zopcuk <keremzopcuk@gmail.com>
2021-08-18 01:30:47 +0300

    Add homepage menu items

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,5 @@
 import { addAfter, insertItem, makeButton, makeImage } from '../utils';
-import homeAboutImg1 from '../img/pexels-daria-shevtsova-1260968.jpg';
-import homeAboutImg2 from '../img/pexels-christina-voinova-7368032.jpg';
-import homeAboutImg3 from '../img/pexels-brett-jordan-825661.jpg';
+import { headerImages, homeMenuItemImgs } from '../imageUrls';
 import '../css/pages/home.scss';
 import header from '../components/header';
 
@@ -22,11 +20,12 @@ makeButton('hero-text', 'Book Now', 'button', 'book-now-btn', 'btn-primary');
 insertItem('home-main', null, 'div', 'home-about-section-wrapper');
 insertItem('home-about-section-wrapper', null, 'div', 'home-about-section');
 insertItem('home-about-section', null, 'div', 'home-about-left');
-makeImage('home-about-left', homeAboutImg1, null, 'home-about-image');
-makeImage('home-about-left', homeAboutImg2, null, 'home-about-image');
-makeImage('home-about-left', homeAboutImg3, null, 'home-about-image');
+makeImage('home-about-left', headerImages[0], null, 'home-about-image');
+makeImage('home-about-left', headerImages[1], null, 'home-about-image');
+makeImage('home-about-left', headerImages[2], null, 'home-about-image');
 insertItem('home-about-section', null, 'div', 'home-about-right');
 insertItem('home-about-right', 'About Us', 'h2', null, 'section-title');
+insertItem('home-about-right', null, 'span', null, 'hr-left');
 insertItem(
   'home-about-right',
   'Fish is one of the most wholesome foods that man can eat. In fact, people have been eating fish throughout human history. These days, many cooks yearn Fish is one of the most wholesome foods that man can eat. In fact,',
@@ -43,8 +42,37 @@ insertItem(
 );
 insertItem('home-about-right', 'Learn More', 'button', null, 'btn-primary');
 
+// Create menu section in home
 insertItem('home-main', null, 'div', 'home-menu-section-wrapper');
 insertItem('home-menu-section-wrapper', null, 'div', 'home-menu-section');
+insertItem('home-menu-section', null, 'div', 'home-menu-header');
+insertItem('home-menu-header', 'Our Menu', 'h2', null, 'section-title');
+insertItem(
+  'home-menu-header',
+  'Fish is one of the most wholesome foods that man can eat. In fact, people have been eating fish throughout human',
+  'p',
+  null,
+  'section-paragraph'
+);
+insertItem('home-menu-header', null, 'hr');
+insertItem('home-menu-section', null, 'div', 'home-menu-items-section');
+const menuItemImageUrls = [];
+for (let i = 1; i < 9; i++) {
+  insertItem('home-menu-items-section', null, 'div', 'home-menu-item-wrapper' + i);
+  makeImage('home-menu-item-wrapper' + i, homeMenuItemImgs[i], null, 'home-about-image');
+  insertItem('home-menu-item-wrapper' + i, 'Pizza Margaritta', 'h3', null, 'home-menu-item-title');
+  insertItem('home-menu-item-wrapper' + i, '€10', 'span', null, 'home-menu-item-price');
+  insertItem(
+    'home-menu-item-wrapper' + i,
+    'Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque',
+    'p',
+    null,
+    'home-menu-item-description'
+  );
+  insertItem('home-menu-item-wrapper' + i, 'Jetzt Bestellen', 'button', null, 'home-menu-item-order-btn');
+}
+insertItem('home-menu-section', 'See More', 'button', null, 'btn-primary');
+
 insertItem('home-main', null, 'div', 'home-whyus-section-wrapper');
 insertItem('home-whyus-section-wrapper', null, 'div', 'home-whyus-section');
 
