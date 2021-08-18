8640467105ae15fcd73024cd8c3d8c01dfeefd51
Kerem Zopcuk <keremzopcuk@gmail.com>
2021-08-18 15:01:02 +0300

    Add why choose us section

---
+++ src/imageUrls.js
@@ -0,0 +1,40 @@
+import homeAboutImg1 from './img/pexels-daria-shevtsova-1260968.jpg';
+import homeAboutImg2 from './img/pexels-christina-voinova-7368032.jpg';
+import homeAboutImg3 from './img/pexels-brett-jordan-825661.jpg';
+
+import homeMenuItemImg1 from './img/homeMenuItem1.jpg';
+import homeMenuItemImg2 from './img/homeMenuItem2.jpg';
+import homeMenuItemImg3 from './img/homeMenuItem3.jpg';
+import homeMenuItemImg4 from './img/homeMenuItem4.jpg';
+import homeMenuItemImg5 from './img/homeMenuItem5.jpg';
+import homeMenuItemImg6 from './img/homeMenuItem6.jpg';
+import homeMenuItemImg7 from './img/homeMenuItem7.jpg';
+import homeMenuItemImg8 from './img/homeMenuItem8.jpg';
+
+import homeServiceItem0 from './img/services/007-consult.png';
+import homeServiceItem1 from './img/services/008-chef.png';
+import homeServiceItem2 from './img/services/011-delivery-truck.png';
+import homeServiceItem3 from './img/services/018-time.png';
+import homeServiceItem4 from './img/services/027-order-food.png';
+import homeServiceItem5 from './img/services/032-dinner.png';
+
+export const headerImages = [homeAboutImg1, homeAboutImg2, homeAboutImg3];
+export const homeMenuItemImgs = [
+  homeMenuItemImg1,
+  homeMenuItemImg2,
+  homeMenuItemImg3,
+  homeMenuItemImg4,
+  homeMenuItemImg5,
+  homeMenuItemImg6,
+  homeMenuItemImg7,
+  homeMenuItemImg8,
+];
+export const homeServiceItemImages = [
+  '',
+  homeServiceItem0,
+  homeServiceItem1,
+  homeServiceItem2,
+  homeServiceItem3,
+  homeServiceItem4,
+  homeServiceItem5,
+];
